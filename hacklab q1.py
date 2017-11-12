40ef07dd2662f0ef64357425ef31ce85169b29ca
Stephen <minecraftyugi@gmail.com>
2017-11-11 20:26:31 -0500

    run function wwith m(n), where n is the input. works in python 2.7

--- hacklab q1.py
+++ hacklab q1.py
@@ -1,7 +1,4 @@
-def s(l):return l if 2>len(l)else l[1::-1]+s(l[2:])
+def t(l):return len(l)/2and l[-2:]+t(l[:-2])or l
 def m(n):
- l=range(1,n+1)
- print l
- l=s(l[::-1])
- print l
- while l!=sorted(l):l=s(l[::-1]);print l
+ l=t(range(1,n+1));print l
+ while~-l[0]:l=t(l);print l
